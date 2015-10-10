11552896d71ad9c9cfff588a26be058cdfb5a9ff
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-10-10 17:10:40 -0400

    add local port to identify different clients in the same computer. When running the client need an arg for the local port:   $ java Client 12345

--- Client.java
+++ Client.java
@@ -3,35 +3,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.BindException;
 
 public class Client {
-  public static BufferedReader connect() throws Exception {
-    Socket s = new Socket("127.0.0.1", 3000);
-    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    return input;
-  }
+    static int localPort;
+    static Socket s;
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader input = connect();
-    String stream = null;
-    int count = 0;
-
-    while (count<10) {
-      stream = input.readLine();
-      if(stream == null){
-        System.out.println("Reconnecting...");
-        Thread.sleep(1000);
-
-        try{
-          input = connect();
-          count = 0;
-        }catch(ConnectException ce){
-          count += 1;
-        }
-
-      }else{
-        System.out.println(stream);
-      }
+    public static BufferedReader connect() throws Exception {
+        if(s != null) s.close();
+        s = new Socket();
+        s.bind(new InetSocketAddress("127.0.0.1", localPort));
+        s.connect(new InetSocketAddress("127.0.0.1", 3000));
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        return input;
     }
-  }
+
+    public static void main(String[] args) throws Exception {
+        localPort = Integer.parseInt(args[0]);
+
+        BufferedReader input = connect();
+        String stream = null;
+        int count = 0;
+
+        while (count<10) {
+          stream = input.readLine();
+          if(stream == null){
+            System.out.println("Reconnecting...");
+            Thread.sleep(1000);
+
+            try{
+              input = connect();
+              count = 0;
+            }catch(ConnectException ce){
+              count += 1;
+            }
+          }else{
+            System.out.println(stream);
+          }
+        }
+    }
 }
